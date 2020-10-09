23e9a85edf55ec408bd8b9ad53fa8a78f2ac1d44
Nengi Tonye <70441869+nengi-tonye@users.noreply.github.com>
2020-10-09 16:50:15 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+makeCacheMatrix: 
+This function creates a special "matrix" object that can cache its inverse.
+cacheSolve: 
+This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -13,3 +14,22 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+x=matrix
+x
+> cachematrix <-c(x=matrix){]}
+Error: unexpected '{' in "cachematrix <-c(x=matrix){"
+> x
+Error: object 'x' not found
+> x <-(x=matrix)
+> x
+function (data = NA, nrow = 1, ncol = 1, byrow = FALSE, dimnames = NULL) 
+{
+    if (is.object(data) || !is.atomic(data)) 
+        data <- as.vector(data)
+    .Internal(matrix(data, nrow, ncol, byrow, dimnames, missing(nrow), 
+        missing(ncol)))
+}
+<bytecode: 0x0000024af0a61c60>
+<environment: namespace:base>
+> cachesolvex <-(x,)
+Error: unexpected ',' in "cachesolvex <-(x,"
